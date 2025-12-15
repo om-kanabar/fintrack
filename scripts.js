7d3e09584b0ad11222af2f2f5d69812c54f25294
Om Kanabar <206570655+om-kanabar@users.noreply.github.com>
2025-12-14 21:25:07 -0600

    Added basic firebase functionality

--- scripts.js
+++ scripts.js
@@ -1,5 +1,45 @@
-const hello = document.getElementById("hello");
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getFirestore, doc, getDoc, collection, getDocs } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
-onload(
-    hello.style.color = "#6987ceff"    
-)
+// Firebase project config
+const firebaseConfig = {
+  apiKey: "AIzaSyD4LF3rVTE4jKzf5lh6UII8SlsifRd2GEw",
+  authDomain: "fintrack-ec50a.firebaseapp.com",
+  projectId: "fintrack-ec50a",
+  storageBucket: "fintrack-ec50a.firebasestorage.app",
+  messagingSenderId: "1286389410",
+  appId: "1:1286389410:web:cb45ddaf8f75128cef21fb",
+  measurementId: "G-BW0G8MP2NN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function readExpenses() {
+    const querySnapshot = await getDocs(collection(db, "expenses"));
+    const tbody = document.querySelector("#table-box tbody");
+    tbody.innerHTML = "";
+    querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        const tr = document.createElement("tr");
+        const indexTd = document.createElement("td");
+        indexTd.scope = "row";
+        indexTd.innerHTML = tbody.children.length + 1;
+
+        const amountTd = document.createElement("td");
+        amountTd.innerHTML = data.amount|| "N/A"; 
+
+        const itemTd = document.createElement("td");
+        itemTd.innerHTML = data.item || "N/A";
+
+        tr.appendChild(indexTd);
+        tr.appendChild(amountTd);
+        tr.appendChild(itemTd);
+
+        tbody.appendChild(tr);
+    });
+}
+
+readExpenses();
